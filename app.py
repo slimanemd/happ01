cdaf39f298e416fb2c4cca436039415b39967ed6
mslim <slimanemd@hotmail.com>
2020-07-26 14:07:21 -0400

    update app data models

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
+import os
 from flask import Flask
+from flask_cors import CORS
 
-app =  Flask(__name__)
+from models import setup_db
 
+def create_app(test_config=None):
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app)
 
-@app.route('/')
-def index():
-    return 'Hello Heroku World 2020'
+    @app.route('/')
+    def get_greeting():
+        excited ='true'   #os.environ['EXCITED']
+        greeting = "Hello"
+        if excited == 'true': greeting = greeting + "!!!!!"
+        return greeting
 
+    @app.route('/coolkids')
+    def be_cool():
+        return "Be cool, man, be coooool! You're almost a FSND grad!"
+
+    return app
+
+app = create_app()
 
 if __name__ == '__main__':
     app.run()
-
